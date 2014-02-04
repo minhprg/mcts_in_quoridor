6605b04eb1f67db49fcc902c8666ee7736bca7fc
Ha Quang Minh <minhprg@gmail.com>
2014-02-04 10:43:17 +0700

    Small fix

--- super_player.py
+++ super_player.py
@@ -23,87 +23,9 @@ from quoridor import *
 import mcts
 
 
-class MyAgent():
+class MyAgent(Agent):
 
     """My Quoridor agent."""
-
-    def successors(self, state):
-        """The successors function must return (or yield) a list of
-        pairs (a, s) in which a is the action played to reach the
-        state s; s is the new state, i.e. a pair (b, p) where
-        b is the new board after the action a has been played and
-        p is the player to play the next move.
-        """
-        board, player = state
-        # TODO
-        # IF in the last row AND found the end game state
-        if (board.goals[player] == 8):
-            last = -1
-        else:
-            last = 1
-
-        manhattan = board.goals[player] - board.pawns[player][0]
-
-        if board.pawns[player][0] == board.goals[player] + last and len(board.get_shortest_path_simplified(player)) == 1 and manhattan == 1:
-            print("Last step!")
-            move = ('P', board.goals[player], board.pawns[player][1])
-            yield (move, (board.clone().play_action(move, player), (player + 1) % 2))
-        else:
-            for move in board.get_legal_pawn_moves(player):
-                yield (move, (board.clone().play_action(move, player), (player + 1) % 2))
-
-            opponent_position = board.pawns[(player + 1) % 2]
-            wall_adjacent_consider_level = 2
-
-            if wall_adjacent_consider_level == 1:
-                for move in board.get_legal_wall_moves(player):
-                    import math
-                    if math.fabs(opponent_position[0] - move[1]) < 2 and math.fabs(opponent_position[1] - move[2]) < 2:
-                        if move[1] - opponent_position[0] < 1 and move[2] - opponent_position[1] < 1:
-
-                            yield (move, (board.clone().play_action(move, player), (player + 1) % 2))
-            else:
-                for move in board.get_legal_wall_moves(player):
-                    import math
-                    if math.fabs(opponent_position[0] - move[1]) < 3 and math.fabs(opponent_position[1] - move[2]) < 3:
-                        if move[1] - opponent_position[0] < 2 and move[2] - opponent_position[1] < 2:
-
-                            yield (move, (board.clone().play_action(move, player), (player + 1) % 2))
-
-
-    def cutoff(self, state, depth):
-        """The cutoff function returns true if the alpha-beta/minimax
-        search has to stop; false otherwise.
-        """
-        board, player = state
-        # TODO
-        if board.is_finished():
-            return True
-
-        if depth == 2:
-            return True
-        return board.is_finished()
-
-    def evaluate(self, state):
-        """The evaluate function must return an integer value
-        representing the utility function of the board.
-        """
-        import math
-        board, player = state
-        # TODO
-        SPP = len(board.get_shortest_path_simplified(player))
-        OPP = len(board.get_shortest_path_simplified((player + 1) % 2))
-        player_manhattan = math.fabs(board.pawns[player][0] - board.goals[player])
-
-        opponent_manhattan = math.fabs(board.pawns[(player + 1) % 2][0] - board.goals[(player + 1) % 2])
-        MDP = (9 - player_manhattan) / 9
-        MDO = (9 - opponent_manhattan) / 9
-
-        wall_left = board.nb_walls[player] - board.nb_walls[(player + 1) % 2]
-
-        score = 275 * (((81 - SPP) / 81) - ((81 - OPP) / 81)) + 129 * (MDP - MDO) + wall_left + random.randrange(1, 2, 1) / 10
-
-        return score
 
     def play(self, percepts, player, step, time_left):
         """This function is used to play a move according
