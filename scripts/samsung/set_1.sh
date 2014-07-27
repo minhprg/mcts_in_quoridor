6a207340fb84242baadd194907dbe8f2803f66d3
Ha Quang Minh <minhprg@gmail.com>
2014-07-27 16:12:30 +0200

    Small fix in script path

--- scripts/samsung/set_1.sh
+++ scripts/samsung/set_1.sh
@@ -2,35 +2,35 @@
 for i in {1..5}
 do
     echo "$i times between OMC(a) - OMC(e)"
-    cd ../bin/ && scala application.Main quoridor 0 0 omc omc a e max max 20 /home/qha/tmp/quoridor/set1/
+    cd ../../bin/ && scala application.Main quoridor 0 0 omc omc a e max max 20 /home/qha/tmp/quoridor/set1/
 done
 
 for i in {1..5}
 do
     echo "$i times between OMC(e) - OMC(a)"
-    cd ../bin/ && scala application.Main quoridor 0 0 omc omc e a max max 20 /home/qha/tmp/quoridor/set1/
+    cd ../../bin/ && scala application.Main quoridor 0 0 omc omc e a max max 20 /home/qha/tmp/quoridor/set1/
 done
 
 for i in {1..5}
 do
     echo "$i times between PBBM(a) - PBBM(e)"
-    cd ../bin/ && scala application.Main quoridor 0 0 pbbm pbbm a e robustmax robustmax 20 /home/qha/tmp/quoridor/set1/
+    cd ../../bin/ && scala application.Main quoridor 0 0 pbbm pbbm a e robustmax robustmax 20 /home/qha/tmp/quoridor/set1/
 done
 
 for i in {1..5}
 do
     echo "$i times between PBBM(e) - PBBM(a)"
-    cd ../bin/ && scala application.Main quoridor 0 0 pbbm pbbm e a robustmax robustmax 20 /home/qha/tmp/quoridor/set1/
+    cd ../../bin/ && scala application.Main quoridor 0 0 pbbm pbbm e a robustmax robustmax 20 /home/qha/tmp/quoridor/set1/
 done
 
 for i in {1..5}
 do
     echo "$i times between UCT(a) - UCT(e)"
-    cd ../bin/ && scala application.Main quoridor 0 0 uct uct a e robust robust 20 /home/qha/tmp/quoridor/set1/
+    cd ../../bin/ && scala application.Main quoridor 0 0 uct uct a e robust robust 20 /home/qha/tmp/quoridor/set1/
 done
 
 for i in {1..5}
 do
     echo "$i times between UCT(a) - UCT(e)"
-    cd ../bin/ && scala application.Main quoridor 0 0 uct uct e a robust robust 20 /home/qha/tmp/quoridor/set1/
+    cd ../../bin/ && scala application.Main quoridor 0 0 uct uct e a robust robust 20 /home/qha/tmp/quoridor/set1/
 done
