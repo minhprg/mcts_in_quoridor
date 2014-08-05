5166bdd12995abb3618227be665658c0274098ea
Ha Quang Minh <minhprg@gmail.com>
2014-08-06 01:03:15 +0200

    Scripts for set 6

--- scripts/samsung/set_6_3.sh
+++ scripts/samsung/set_6_3.sh
@@ -5,11 +5,11 @@ cd ../../bin/
 for i in 1 2 3 4 5
 do
     echo "$i times between pbbmq(e) RobustMax - Minimax"
-    scala application.Main quoridor mcts minimax 0 0 pbbmq pbbmq e e robustmax robustmax 30 /home/qha/tmp/quoridor/set6/
+    scala application.Main quoridor mcts minimax 0 0 pbbmq pbbmq e e robustmax robustmax 20 /home/qha/tmp/quoridor/set6/
 done
 
 for i in 1 2 3 4 5
 do
     echo "$i times between Minimax - pbbmq(e) RobustMax"
-    scala application.Main quoridor minimax mcts 0 0 pbbmq pbbmq e e robustmax robustmax 30 /home/qha/tmp/quoridor/set6/
+    scala application.Main quoridor minimax mcts 0 0 pbbmq pbbmq e e robustmax robustmax 20 /home/qha/tmp/quoridor/set6/
 done
