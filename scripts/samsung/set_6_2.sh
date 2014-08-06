4204fd0f06f76a084b17780d57dc6f229ca8f35e
Ha Quang Minh <minhprg@gmail.com>
2014-08-06 02:45:28 +0200

    Change script

--- scripts/samsung/set_6_2.sh
+++ scripts/samsung/set_6_2.sh
@@ -5,11 +5,11 @@ cd ../../bin/
 for i in 1 2
 do
     echo "$i times between uctq(e) Max - Minimax"
-    scala application.Main quoridor mcts minimax 0 0 uctq uctq e e robust robust 180 /home/qha/tmp/quoridor/set6/
+    scala application.Main quoridor mcts minimax 0 0 uctq uctq e e robust robust 300 /home/qha/tmp/quoridor/set6/
 done
 
 for i in 1 2
 do
     echo "$i times between Minimax - uctq(e) Max"
-    scala application.Main quoridor minimax mcts 0 0 uctq uctq e e robust robust 180 /home/qha/tmp/quoridor/set6/
+    scala application.Main quoridor minimax mcts 0 0 uctq uctq e e robust robust 300 /home/qha/tmp/quoridor/set6/
 done
