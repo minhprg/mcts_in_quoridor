cc28c3b8cbf0c39e8ddcab8f06a43d2d10fe8ff5
Ha Quang Minh <minhprg@gmail.com>
2014-08-08 10:22:02 +0200

    Update for set 6

--- scripts/samsung/set_6_1.sh
+++ scripts/samsung/set_6_1.sh
@@ -5,11 +5,11 @@ cd ../../bin/
 for i in 1 2 3 4 5
 do
     echo "$i times between omcq(e) Max - Minimax"
-    scala application.Main quoridor mcts minimax 0 0 omcq minimax e e max max 30 /home/qha/tmp/quoridor/set6/
+    scala application.Main quoridor mcts minimax 0 0 omcq minimax e e max max 20 /home/qha/tmp/quoridor/set6/
 done
 
 for i in 1 2 3 4 5
 do
     echo "$i times between Minimax - omcq(e) Max"
-    scala application.Main quoridor minimax mcts 0 0 minimax omcq e e max max 30 /home/qha/tmp/quoridor/set6/
+    scala application.Main quoridor minimax mcts 0 0 minimax omcq e e max max 20 /home/qha/tmp/quoridor/set6/
 done
